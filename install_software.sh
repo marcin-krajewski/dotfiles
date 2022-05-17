47aef7831cacbb314c6a4a6992a06cbcf322a870
Marcin Krajewski <Marcin.Krajewski@smartstream-stp.com>
2022-05-17 17:57:03 +0200

    different ways of software install

--- install_software.sh
+++ install_software.sh
@@ -1,27 +1,18 @@
 # paci         - install one or more packages
 # pacu         - upgrade all packages to their newest version
 
+packages=(vim vifm git termite zsh mc)
+
 if [ -e "/usr/bin/apt-get" ]; then # Apt-based distros (Debian, Ubuntu, etc.)
-  aptget="/usr/bin/apt-get"
-  sudoaptget="sudo $aptget"
-  alias paci="$sudoaptget install"
-  alias pacu="$sudoaptget update"
+  sudo apt-get update
+  sudo apt-get install "${packages[@]}"
 elif [ -e "/usr/bin/pacman" ]; then # Arch Linux
-  pacman="/usr/bin/pacman"
-  sudopacman="sudo $pacman"
-  alias paci="$pacman -S"
-  alias pacu="$pacman -Syu"
+  sudo pacman -Syu
+  sudo pacman -S "${packages[@]}"
 elif [ -e "/usr/bin/yum" ]; then # RPM-based distros
-  yum="/usr/bin/yum"
-  sudoyum="sudo $yum"
-  alias paci="$sudoyum install"
-  alias pacu="$sudoyum update"
+  sudo yum update
+  sudo yum install "${packages[@]}"
 elif [ -e "/usr/local/bin/brew" ]; then # homebrew
-  brew="/usr/local/bin/brew"
-  alias paci="$brew install"
-  alias pacu="$brew update"
+  sudo brew update
+  sudo brew install "${packages[@]}"
 fi
-
-
-pacu
-paci vim vifm git termite mc
