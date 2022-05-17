29dc899d2aa4111791a699d9d6bf60066897d236
Marcin Krajewski <Marcin.Krajewski@smartstream-stp.com>
2022-05-17 18:11:19 +0200

    change ls to echo

--- install_software.sh
+++ install_software.sh
@@ -5,7 +5,7 @@ packages=(vim vifm git termite zsh mc)
 
 if [ -e "/usr/bin/apt-get" ]; then # Apt-based distros (Debian, Ubuntu, etc.)
   apt-get update
-  ls $packages | xargs -n 1 apt-get install
+  echo $packages | xargs -n 1 apt-get install
 elif [ -e "/usr/bin/pacman" ]; then # Arch Linux
   pacman -Syu
   pacman -S "${packages[@]}"
